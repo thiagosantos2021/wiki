32183f45cc55378a8bd1d2d48f3c1b0a2da29f5d
Thiago Santos <thiago_santos_1990@hotmail.com>
2023-06-08 10:58:23 -0300

    efeuando alterações

---
+++ src/components/ItemRepo/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { ItemContainer } from './styles';
+
+function ItemRepo({repo, handleRemoveRepo}) {
+
+    const handleRemove = () => {
+        handleRemoveRepo(repo.id)
+    }
+
+    return (
+        <ItemContainer onClick={handleRemove}>
+            <h3>{repo.name}</h3>
+            <p>{repo.full_name}</p>
+            <a href={repo.html_url} target="_blank" rel="noreferrer">Ver Repositório</a><br />
+            <a onClick={handleRemove} href="/" className="remover" rel="noreferrer">Remover</a>
+            <hr />
+        </ItemContainer>
+    )
+}
+
+export default ItemRepo;
